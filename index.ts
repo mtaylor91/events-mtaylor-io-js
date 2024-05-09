ad0eee543e1987b5da1a96ec81b2b53d40c1bfd7
Mike Taylor <mike.charles.taylor@gmail.com>
2024-05-08 21:48:28 -0600

    Wrap Socket in Client and add http(s) request helper

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
+import sodium from 'libsodium-wrappers-sumo';
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
 import IAM from 'iam-mtaylor-io-js';
 import type { User } from 'iam-mtaylor-io-js';
 import { parse as parseUUID, v4 as uuidv4 } from 'uuid';
 
-const DEFAULT_URL = "wss://events.mtaylor.io";
+const DEFAULT_HOST = "events.mtaylor.io";
+const DEFAULT_SECURE = true;
 
 
 export interface ClientHello {
@@ -12,7 +16,88 @@ export interface ClientHello {
 }
 
 
-export default class Socket {
+export default class Client {
+  public url: string;
+  public host: string;
+  public secure: boolean;
+  public iam: IAM;
+  public socket: Socket;
+
+  constructor(iam: IAM, host: string = DEFAULT_HOST, secure: boolean = DEFAULT_SECURE) {
+    this.iam = iam;
+    this.host = host;
+    this.secure = secure;
+    this.url = `${secure ? 'https' : 'http'}://${host}`;
+    this.socket = new Socket(iam, host, secure);
+  }
+
+  public async connect(): Promise<Socket> {
+    return await this.socket.connect();
+  }
+
+  async request(
+    method: string,
+    path: string,
+    query: string | null = null,
+    body: any | null = null,
+  ): Promise<AxiosResponse> {
+    const publicKey = this.iam.publicKey;
+    const secretKey = this.iam.secretKey;
+    const sessionToken = this.iam.sessionToken;
+
+    if (!publicKey || !secretKey || !sessionToken) {
+      throw new Error('IAM Client must be logged in');
+    }
+
+    const url = `${this.url}${path}${query ? `?${query}` : ''}`;
+    const requestId = uuidv4();
+    const publicKeyBase64 = sodium.to_base64(publicKey, sodium.base64_variants.URLSAFE);
+    const signature = sodium.to_base64(sodium.crypto_sign_detached(
+      this.requestStringToSign(method, path, query, requestId, sessionToken),
+      secretKey), sodium.base64_variants.URLSAFE);
+
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Signature ${signature}`,
+      'X-MTaylor-IO-User-ID': this.iam.userId,
+      'X-MTaylor-IO-Public-Key': publicKeyBase64,
+      'X-MTaylor-IO-Request-ID': requestId,
+      'X-MTaylor-IO-Session-Token': sessionToken,
+    };
+
+    const response = await axios.request({
+      method,
+      url,
+      headers,
+      data: body,
+    });
+
+    return response;
+  }
+
+  requestStringToSign(
+    method: string,
+    path: string,
+    query: string | null,
+    requestId: string,
+    sessionToken: string,
+  ): Uint8Array {
+    const parts = [
+      method,
+      this.host,
+      path,
+      query || '',
+      requestId,
+      sessionToken,
+    ];
+
+    return sodium.from_string(parts.join('\n'));
+  }
+}
+
+
+export class Socket {
   public connected: boolean;
 
   public userHandlers: Map<string, ((event: MessageEvent) => void)[]>;
@@ -24,12 +109,12 @@ export default class Socket {
   private iam: IAM;
   private user: null | User;
 
-  constructor(iam: IAM, url: string = DEFAULT_URL) {
+  constructor(iam: IAM, host: string = DEFAULT_HOST, secure: boolean = DEFAULT_SECURE) {
     this.connected = false;
     this.socket = null;
     this.user = null;
     this.iam = iam;
-    this.url = url;
+    this.url = `${secure ? 'wss' : 'ws'}://${host}`;
     this.userHandlers = new Map();
     this.groupHandlers = new Map();
     this.sessionHandlers = [];
